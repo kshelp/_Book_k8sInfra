a74ed30a11662cf3b85ddc70adb8528ed5c80a1b
Max Lee <kshelp@naver.com>
2024-10-20 07:25:53 +0900

    Update build-in-host.sh

--- ch4/4.5.3/build-in-host.sh
+++ ch4/4.5.3/build-in-host.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-docker build --no-cache -t react-app .
+docker build --no-cache -t 192.168.1.10:8443/react-app .
+docker push 192.168.1.10:8443/react-app
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
